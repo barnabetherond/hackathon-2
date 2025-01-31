ea14a77b4b00679728a246ec5a1091d3b490444b
Julien Couturier <julien.couturier@etu.minesparis.psl.eu>
2025-01-31 14:17:03 +0100

    création_ui

--- julien/src/main.py
+++ julien/src/main.py
@@ -1,26 +1,30 @@
 import flet as ft
 
+class Board:
+    def __init__(self, chaine):
+        self.plateau = chaine
+    
+    def __eq__(self, chaine):
+        return self.plateau == chaine.plateau
 
-def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
+header = ft.Text(value="Bienvenue sur CCsolution2025byBaJu2Os (taquin resolver) !", color="white", weight=ft.FontWeight.BOLD)
+squares = [ft.Image(src=f"{i}.jpg",width=10,height=10,border_radius=10, fit=ft.ImageFit.COVER) for i in range(1,9)]
+squares.append(ft.Container(content=ft.Text(value="-", color="white",weight=ft.FontWeight.BOLD,size=24),width=10,height=10,border_radius=10,bgcolor='#32353d',alignment=ft.Alignment(0, 0)))
+bottom = [ft.Image(src="shuf.png",width=50,height=50),ft.Image(src="sol.png",width=50,height=50)]
 
-    def increment_click(e):
-        counter.data += 1
-        counter.value = str(counter.data)
-        counter.update()
+def reset(board):
+    for i, square in zip(board, squares):
+        square.value = i
 
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
-    )
+def main(page):
+    page.window_width = 600
+    page.window_height = 400
     page.add(
-        ft.SafeArea(
-            ft.Container(
-                counter,
-                alignment=ft.alignment.center,
-            ),
-            expand=True,
-        )
-    )
-
+        ft.Column([header]),
+        ft.GridView(squares, expand=False ,max_extent=100,spacing=10,run_spacing=10),
+        ft.Row(bottom,alignment=ft.MainAxisAlignment.CENTER, spacing=50)
+            )
 
 ft.app(main)
+
+#ft.Text(value=str(i), color="white",weight=ft.FontWeight.BOLD,size=24),width=10,height=10,border_radius=10,bgcolor=ft.colors.BLUE,alignment=ft.Alignment(0, 0)
